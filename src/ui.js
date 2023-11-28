afa55a30f413296fda77c63b72f6d71fd2274448
CLOWNAKIO <dead.kanji@mail.ru>
2023-11-28 11:38:16 +0300

    feat(ui): added class for perform dom elements

---
+++ src/ui.js
@@ -0,0 +1,20 @@
+export class UI {
+  static createElement(tagName, properties) {
+    const element = document.createElement(tagName);
+    const { class: className, id, style, text } = properties;
+    element.className = className ? className.join(" ") : "";
+    element.id = id ? id : "";
+    element.style = style ? style : "";
+    element.textContent = text ? text : "";
+
+    return element;
+  }
+
+  static getElement(selector) {
+    return document.querySelector(selector);
+  }
+
+  static append(parent, childs) {
+    for (let child in childs) parent.appendChild(childs[child]);
+  }
+}
